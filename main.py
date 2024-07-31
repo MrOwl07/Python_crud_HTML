6b53965426b09ee08e34dc9fb5bbad46b2011532
esteman500@gmail.com <esteman500@gmail.com>
2024-07-31 00:51:13 -0500

    conexion en base de datos

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
-
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+#conexion a base de datos
+mysql = MySQL()
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'dbcrud'
+mysql.init_app(app)
 
 @app.route('/')
 def index():
@@ -9,7 +17,17 @@ def index():
 
 @app.route('/clientes')
 def index_clientes():
-    return render_template('modulos/clientes/index.html')
+    
+
+    sql = 'SELECT * FROM db_clientes'
+    conex = mysql.connection
+    cursor = conex.cursor()
+    cursor.execute(sql)
+    clientes = cursor.fetchall()
+    conex.commit()
+    return render_template('modulos/clientes/index.html', clientes = clientes)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
