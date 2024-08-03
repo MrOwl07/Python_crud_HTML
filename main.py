a65a9c8fc7dee8449c16b8db21970391753d2c41
esteman500@gmail.com <esteman500@gmail.com>
2024-08-03 00:43:51 -0500

    Vistas creadas de edit/delete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -9,6 +9,7 @@ app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'dbcrud'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql.init_app(app)
 
 @app.route('/')
@@ -27,7 +28,52 @@ def index_clientes():
     conex.commit()
     return render_template('modulos/clientes/index.html', clientes = clientes)
 
+@app.route('/cliente/create')
+def create():
+    return render_template('modulos/clientes/create.html')
 
+@app.route('/clientes/create/guardar', methods = ['POST'])
+def clientes_save():
+    nombre = request.form['nombre']
+    telefono = request.form['telefono']
+    fecha = request.form['fecha']
+    sql = 'INSERT INTO db_clientes(nombre, telefono, fecha) VALUES (%s,%s,%s)'
+    datos = (nombre,telefono,fecha)
+    conexion = mysql.connection
+    cursor = conexion.cursor()
+    cursor.execute(sql, datos)
+    conexion.commit()
+    return redirect('/clientes')
+@app.route('/clientes/edit/<int:id>')
+def clientes_editar(id):
+    conexion = mysql.connection
+    cursor = conexion.cursor()
+    cursor.execute('SELECT * FROM db_clientes WHERE id=%s',(id,))
+    clientes = cursor.fetchone()
+    conexion.commit()
+    return render_template('modulos/clientes/edit.html', clientes=clientes)
+
+@app.route('/clientes/edit/actualizar', methods=['POST'])
+def clientes_actualizar():
+    id = request.form['txtid']
+    nombre = request.form['nombre']
+    telefono = request.form['telefono']
+    fecha = request.form['fecha']
+    slq = 'UPDATE db_clientes SET nombre = %s, telefono = %s, fecha = %s WHERE id = %s'
+    datos = ( nombre,telefono,fecha,id)
+    conexion = mysql.connection
+    cursor = conexion.cursor()
+    cursor.execute(slq,datos)
+    conexion.commit()
+    return redirect('/clientes')
+
+@app.route('/clientes/borrar/<int:id>')
+def clientes_borrar(id):
+    conexion = mysql.connection
+    cursor = conexion.cursor()
+    cursor.execute('DELETE FROM db_clientes WHERE id=%s',(id,))
+    conexion.commit()
+    return redirect('/clientes')
 
 if __name__ == '__main__':
     app.run(debug=True)
